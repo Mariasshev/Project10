54cdbb6caa50c72bf820e86ce521d6b988b85f5b
Maria Shevchenko <sevcenkomaria72@gmail.com>
2024-02-19 18:53:30 +0200

    result

--- Project10/resource.h
+++ Project10/resource.h
@@ -31,6 +31,7 @@
 #define IDC_EDIT2                       1018
 #define IDC_SPIN1                       1019
 #define IDC_PROGRESS1                   1020
+#define IDC_LIST1                       1021
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -40,7 +41,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1021
+#define _APS_NEXT_CONTROL_VALUE         1022
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
